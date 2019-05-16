79ebc0c1c55698bd84d15e10b8eb7990ee46e0bc
Daniel Goldman <dzgoldman@wesleyan.edu>
2019-05-16 08:09:47 -0400

    add appjs

---
+++ app.js
@@ -0,0 +1,10 @@
+// takes an array as an arg, returns the last element
+const lastInArray =  (arr)=>{
+
+}
+
+
+// take an array of numbers, returns a boolean indicating whether or not the array includes the number 23 (at least once)
+const has23 = (arr)=>{
+
+}
